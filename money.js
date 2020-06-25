b35ee0b519b31709b33deff021de79441d73f139
Nicolas <nicolas.strike1234@hotmail.com>
2020-06-24 23:46:08 -0300

    Troca metodo para orientado a objetos, falta alguns detalhes

--- money.js
+++ money.js
@@ -1,48 +1,43 @@
-const money = (eloAtual, divisaoAtual, eloDesejado, divisaoDesejada, duoboost) => {
-  let priceElo = [5, 10, 20, 25, 35, 65];
-  let priceEloDuo = [5, 10, 20, 25, 35, 65];
-
-  const elos = ["ferro", "bronze", "prata", "gold", "platina", "diamante"];
-  let atual = [eloAtual, divisaoAtual];
-  const desejado = [eloDesejado, divisaoDesejada];
-  let valorDoEloAtual;
-  let priceTotal = 0;
-  let eloAtualIndex;
-
-
-  // Fara isso enquanto nao estiver no elo desejado, 
-  while (atual[0] !== desejado[0]) {
-    // Verifica qual o elo (ferro,bronze...) e atribui o preço referente
-    for (let i = 0; i < elos.length; i++) {
-      if (atual[0] === elos[i]) {
-        valorDoEloAtual = priceElo[i];
-        // i representa ndice do preço referente, sera incrementado futuramente quando o usuario subir de elo
-        eloAtualIndex = i;
-      }
-    }
-    // preço recebe o valor por divisao do elo atua e multiplica por quantas vezes ele precisara subir
-    priceTotal += valorDoEloAtual * atual[1];
-    // Como supracitado era incrimentar pois aqui o usuario ja subiu de elo
-    eloAtualIndex++;
-    // Elo atual passa a ser o proximo na lista
-    atual[0] = elos[eloAtualIndex];
-    // Como passou de elo, automaticamente passou para a divisão 4
-    atual[1] = 4;
-  }
-
-// Pela segunda vez, verifica qual o elo e atribui o preço referente 
-  for (let i = 0; i < elos.length; i++) {
-    if (atual[0] === elos[i]) {
-      valorDoEloAtual = priceElo[i];
-      eloAtualIndex = i;
-    }
-  }
-    // Aqui ele ja estara no elo desejado, mas não necessariamente divisão desejada
-  if (atual[0] === desejado[0]) {
-    // Quantidades de divisão que ainda precisa subir
-    let divisoes = atual[1] - desejado[1];
-    priceTotal = priceTotal + divisoes * valorDoEloAtual;
-  }
-  console.log(priceTotal);
+let job = {
+  nome: 'Degaleto',
+  ligaAtual: 'prata',
+  precoDaLigaAtual: 0,
+  divisaoAtual: 4,
+  ligaDesejada: 'platina',
+  divisaoDesejada: 4,
+  duoBoost: false,
+  divisoesParaSubir: 0,
+  preco: 0,
 };
-money("prata", 4, "platina", 4, true);
+
+let info = { 
+  ligas: ["ferro", "bronze", "prata", "gold", "platina", "diamante"],
+  precoPorLiga: [5, 10, 20, 25, 35, 65],
+  precoPorEloDuoBoost: [5, 10, 20, 25, 35, 65],
+};
+
+while(job.ligaAtual !== job.ligaDesejada) {
+let posicaoDoEloAtual;
+ for (let i = 0; i < info.ligas.length; i++){
+    if (job.ligaAtual === info.ligas[i] ){
+      job.precoDaLigaAtual = info.precoPorLiga[i]
+      posicaoDoEloAtual = i;
+  }
+}
+job.preco += job.precoDaLigaAtual * job.divisaoAtual;
+posicaoDoEloAtual++;
+job.ligaAtual = info.ligas[posicaoDoEloAtual];
+job.divisaoAtual = 4;
+}
+
+for (let i = 0; i < info.ligas.length; i++){
+  if (job.ligaAtual === info.ligas[i] ){
+    job.precoDaLigaAtual = info.precoPorLiga[i]
+    posicaoDoEloAtual = i;
+}
+}
+if (job.ligaAtual === job.ligaDesejada) {
+  job.divisoesParaSubir = job.divisaoAtual - job.divisaoDesejada;
+  job.preco += job.divisoesParaSubir * job.precoDaLigaAtual
+}
+console.log(job.preco);
